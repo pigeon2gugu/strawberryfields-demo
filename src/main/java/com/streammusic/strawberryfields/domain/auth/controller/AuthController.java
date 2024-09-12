e5bb545d4add1c79e58146afa379d05feaf4d9a1
pigeon2gugu <khn1135@gmail.com>
2024-09-12 16:18:47 +0900

    feat : login 서비스 및 컨트롤러 추가

---
+++ src/main/java/com/streammusic/strawberryfields/domain/auth/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.streammusic.strawberryfields.domain.auth.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.streammusic.strawberryfields.domain.auth.service.AuthService;
+import com.streammusic.strawberryfields.domain.auth.service.dto.LoginDto;
+import com.streammusic.strawberryfields.domain.auth.service.dto.RegenerateTokenDto;
+import com.streammusic.strawberryfields.domain.user.service.dto.RequestUser;
+import com.streammusic.strawberryfields.global.common.ApiResult;
+import com.streammusic.strawberryfields.global.security.Authenticated;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/auth")
+@Validated
+public class AuthController {
+	private final AuthService authService;
+
+	@PostMapping("/login")
+	public ApiResult<LoginDto.Response> login(@RequestBody @Valid LoginDto.Request loginRequest) {
+		return ApiResult.ok(authService.login(loginRequest));
+	}
+
+	@GetMapping("/refresh")
+	public ApiResult<RegenerateTokenDto.Response> regenerateToken(
+		@RequestHeader("refreshToken") String refreshToken,
+		@Authenticated RequestUser requestUser) {
+
+		return ApiResult.ok(authService.regenerateToken(refreshToken, requestUser.getUserId()));
+	}
+}
