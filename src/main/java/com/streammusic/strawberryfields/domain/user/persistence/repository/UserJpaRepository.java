5ef6152d3e219eadc0bc7edfacad1007fe1400c4
pigeon2gugu <khn1135@gmail.com>
2024-09-14 13:37:32 +0900

    feat : 피칭 추가시 유저 검증을 위한 역할, id로 유저 조회 서비스 추가

--- src/main/java/com/streammusic/strawberryfields/domain/user/persistence/repository/UserJpaRepository.java
+++ src/main/java/com/streammusic/strawberryfields/domain/user/persistence/repository/UserJpaRepository.java
@@ -5,9 +5,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.streammusic.strawberryfields.domain.user.persistence.domain.User;
+import com.streammusic.strawberryfields.domain.user.persistence.enums.Role;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
 	Optional<User> findById(Long userId);
+
+	Optional<User> findByIdAndRole(Long userId, Role role);
 
 	Optional<User> findByEmail(String email);
 
--- src/main/java/com/streammusic/strawberryfields/domain/user/persistence/repository/query/UserQueryRepository.java
+++ src/main/java/com/streammusic/strawberryfields/domain/user/persistence/repository/query/UserQueryRepository.java
@@ -3,9 +3,12 @@ package com.streammusic.strawberryfields.domain.user.persistence.repository.quer
 import java.util.Optional;
 
 import com.streammusic.strawberryfields.domain.user.persistence.domain.User;
+import com.streammusic.strawberryfields.domain.user.persistence.enums.Role;
 
 public interface UserQueryRepository {
 	Optional<User> findById(Long userId);
+
+	Optional<User> findByIdAndRole(Long userId, Role role);
 
 	Optional<User> findByEmail(String email);
 
