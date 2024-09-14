09149b645ab5c607b46618403afb5bd877470373
pigeon2gugu <khn1135@gmail.com>
2024-09-14 11:33:03 +0900

    fix : 누락된 query method 명시

--- src/main/java/com/streammusic/strawberryfields/domain/track/persistence/repository/TrackJpaRepository.java
+++ src/main/java/com/streammusic/strawberryfields/domain/track/persistence/repository/TrackJpaRepository.java
@@ -1,11 +1,17 @@
 package com.streammusic.strawberryfields.domain.track.persistence.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.streammusic.strawberryfields.domain.track.persistence.domain.Track;
+import com.streammusic.strawberryfields.domain.track.service.dto.TrackListDto;
+import com.streammusic.strawberryfields.domain.user.persistence.domain.User;
 
 public interface TrackJpaRepository extends JpaRepository<Track, Long> {
 
 	public Track save(Track track);
+
+	Page<TrackListDto.Response> findAllByUser(Pageable pageable, User user);
 
 }
