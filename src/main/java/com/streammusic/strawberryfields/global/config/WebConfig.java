537b2a5c1a2fd65650e74befa25f724521b5a6c3
pigeon2gugu <khn1135@gmail.com>
2024-09-15 21:37:26 +0900

    fix : page 직렬화 config 설정

--- src/main/java/com/streammusic/strawberryfields/global/config/WebConfig.java
+++ src/main/java/com/streammusic/strawberryfields/global/config/WebConfig.java
@@ -1,8 +1,11 @@
 package com.streammusic.strawberryfields.global.config;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.*;
+
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class WebConfig implements WebMvcConfigurer {
 	private final HandlerMethodArgumentResolver authenticatedArgumentResolver;
 
