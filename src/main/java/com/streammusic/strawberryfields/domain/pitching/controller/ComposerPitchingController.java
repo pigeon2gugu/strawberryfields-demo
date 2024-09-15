13261f490dac997350a5b3e5d00b32766ac68a2e
pigeon2gugu <khn1135@gmail.com>
2024-09-15 23:15:08 +0900

    feat : 기획사 및 작곡가 피칭 내역 조회 기능 추가

--- src/main/java/com/streammusic/strawberryfields/domain/pitching/controller/ComposerPitchingController.java
+++ src/main/java/com/streammusic/strawberryfields/domain/pitching/controller/ComposerPitchingController.java
@@ -1,12 +1,18 @@
 package com.streammusic.strawberryfields.domain.pitching.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.streammusic.strawberryfields.domain.pitching.service.command.PitchingCommandService;
+import com.streammusic.strawberryfields.domain.pitching.service.dto.ComposerPitchingListDto;
 import com.streammusic.strawberryfields.domain.pitching.service.dto.CreatePitchingDto;
+import com.streammusic.strawberryfields.domain.pitching.service.query.PitchingQueryService;
 import com.streammusic.strawberryfields.domain.user.service.dto.RequestUser;
 import com.streammusic.strawberryfields.global.common.ApiResult;
 import com.streammusic.strawberryfields.global.security.Authenticated;
@@ -16,14 +22,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("api/v1/composer/pitching")
 @RestController
-public class PitchingController {
+public class ComposerPitchingController {
 	private final PitchingCommandService pitchingCommandService;
+	private final PitchingQueryService pitchingQueryService;
 
 	@PostMapping()
 	public ApiResult<CreatePitchingDto.Response> createPitching(@RequestBody CreatePitchingDto.Request request,
 		@Authenticated RequestUser requestUser) {
 
 		return ApiResult.ok(pitchingCommandService.createPitching(request, requestUser.getUserId()));
+	}
+
+	@GetMapping()
+	public ApiResult<Page<ComposerPitchingListDto.Response>> getPitchings(@PageableDefault(size = 10) Pageable pageable,
+		@Authenticated RequestUser requestUser) {
+
+		return ApiResult.ok(pitchingQueryService.getComposerPitchings(pageable, requestUser.getUserId()));
 	}
 
 }
--- src/main/java/com/streammusic/strawberryfields/domain/pitching/service/query/PitchingQueryService.java
+++ src/main/java/com/streammusic/strawberryfields/domain/pitching/service/query/PitchingQueryService.java
@@ -1,14 +1,23 @@
 package com.streammusic.strawberryfields.domain.pitching.service.query;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.streammusic.strawberryfields.domain.pitching.persistence.domain.Pitching;
 import com.streammusic.strawberryfields.domain.pitching.persistence.repository.query.PitchingQueryRepository;
+import com.streammusic.strawberryfields.domain.pitching.service.dto.AgencyPitchingListDto;
+import com.streammusic.strawberryfields.domain.pitching.service.dto.ComposerPitchingListDto;
 import com.streammusic.strawberryfields.domain.track.persistence.domain.Track;
 import com.streammusic.strawberryfields.domain.user.persistence.domain.User;
+import com.streammusic.strawberryfields.domain.user.service.query.UserQueryService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,10 +25,49 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PitchingQueryService {
+	private static final String TIME_ZONE = "Asia/Seoul";
+
 	private final PitchingQueryRepository pitchingQueryRepository;
+	private final UserQueryService userQueryService;
 
 	public Optional<Pitching> findByTrackAndAgency(Track track, User agency) {
-		
+
 		return pitchingQueryRepository.findByTrackAndUser(track, agency);
 	}
+
+	public Page<AgencyPitchingListDto.Response> getAgencyPitchings(Pageable pageable, Long userId) {
+		User agency = userQueryService.getById(userId);
+		Page<AgencyPitchingListDto.Response> pitchings = pitchingQueryRepository.findAllByAgency(pageable, agency);
+
+		List<AgencyPitchingListDto.Response> transformedTimeResult = pitchings.getContent().stream()
+			.map(response -> new AgencyPitchingListDto.Response(
+				response.getId(),
+				response.getTitle(),
+				response.getArtist(),
+				Instant.parse(response.getCreatedAt()),
+				TIME_ZONE
+			))
+			.collect(Collectors.toList());
+
+		return new PageImpl<>(transformedTimeResult, pageable, pitchings.getTotalElements());
+	}
+
+	public Page<ComposerPitchingListDto.Response> getComposerPitchings(Pageable pageable, Long userId) {
+		User composer = userQueryService.getById(userId);
+		Page<ComposerPitchingListDto.Response> pitchings = pitchingQueryRepository.findAllByComposer(pageable,
+			composer);
+
+		List<ComposerPitchingListDto.Response> transformedTimeResult = pitchings.getContent().stream()
+			.map(response -> new ComposerPitchingListDto.Response(
+				response.getId(),
+				response.getTitle(),
+				response.getCompany(),
+				Instant.parse(response.getCreatedAt()),
+				TIME_ZONE
+			))
+			.collect(Collectors.toList());
+
+		return new PageImpl<>(transformedTimeResult, pageable, pitchings.getTotalElements());
+	}
+
 }
