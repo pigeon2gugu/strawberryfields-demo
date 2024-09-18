d19a32b1b943e4c0de30911189da5c3d37335d03
pigeon2gugu <khn1135@gmail.com>
2024-09-19 00:23:55 +0900

    fix : role에 따른 redirect page 구분을 위한 로그인 시 role 반환 추가

--- src/main/java/com/streammusic/strawberryfields/domain/auth/service/dto/LoginDto.java
+++ src/main/java/com/streammusic/strawberryfields/domain/auth/service/dto/LoginDto.java
@@ -1,5 +1,7 @@
 package com.streammusic.strawberryfields.domain.auth.service.dto;
 
+import com.streammusic.strawberryfields.domain.user.persistence.enums.Role;
+
 import jakarta.validation.constraints.NotBlank;
 
 public class LoginDto {
@@ -7,6 +9,6 @@ public class LoginDto {
 	public record Request(@NotBlank String email, String password) {
 	}
 
-	public record Response(String accessToken, String refreshToken, Long userId) {
+	public record Response(String accessToken, String refreshToken, Long userId, Role role) {
 	}
 }
